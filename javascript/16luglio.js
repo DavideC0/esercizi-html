961d19ba9a503f6a2c5155df7fdbdc05d3302528
Nio4124 <antoniozottola41204@gmail.com>
2024-07-16 10:03:57 +0200

    i tipi delle variabili possono cambiare

--- javascript/16luglio.js
+++ javascript/16luglio.js
@@ -4,3 +4,7 @@ A = 3;
 B = "buongiorno a tutti";
 console.log("Il tipo della var A e' " + typeof(A) + "\n");
 console.log("Il tipo della var B e' " + typeof (B) + "\n");
+
+console.log("Il tipo di A prima del cambiamento è: " + typeof(A) + "\n");
+A = "L'inverno è meglio"
+console.log("Il tipo di A dopo il cambiamento è: " + typeof(A) + "\n");
