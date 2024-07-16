6bf63efc8852eea2bc81c5c1fb10cf06598d05e7
Nio4124 <antoniozottola41204@gmail.com>
2024-07-16 10:26:34 +0200

    ricerca di una lettera

--- javascript/16luglio.js
+++ javascript/16luglio.js
@@ -5,6 +5,22 @@ B = "buongiorno a tutti";
 console.log("Il tipo della var A e' " + typeof(A) + "\n");
 console.log("Il tipo della var B e' " + typeof (B) + "\n");
 
-console.log("Il tipo di A prima del cambiamento è: " + typeof(A) + "\n");
+console.log("Il tipo di A prima del cambiamento è: " + typeof(A));
 A = "L'inverno è meglio"
-console.log("Il tipo di A dopo il cambiamento è: " + typeof(A) + "\n");
+console.log("Il tipo di A dopo il cambiamento è: " + typeof(A));
+
+var iPosizione;
+var sMiaStringa;
+sMiaStringa = "paperino"
+iPosizione = sMiaStringa.indexOf("z");
+if((iPosizione == -1) || (iPosizione > 3))
+	console.log("La z non c'e' nei primi 4 caratteri")
+else
+	console.log("La z c'e' nei primi 4 caratteri");
+
+var sStringa = "francesco totti"
+var iPosizione = sStringa.indexOf("f")
+if (iPosizione == -1)
+    console.log("La letterea f non si trova nella stringa " + sStringa)
+else
+    console.log("La letterea f si trova nella stringa " + sStringa + " al carattere numero " + iPosizione)
